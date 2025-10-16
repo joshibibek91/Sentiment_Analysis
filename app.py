7bfb7c6b125fc49493197b319116728465d3fd99
Bibek <bibek.joshi91@gmail.com>
2025-10-16 10:28:53 +0545

    first commit

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import pickle
+import numpy as np
+st.title("Sentiment Analysis App ")
+
+dbfile = open('LogisticRegression.pickle', 'rb')
+model = pickle.load(dbfile)
+
+#input form
+
+text = st.text_input("Enter your review = ")
+review_data = {'predict_sentiment':[text]}
+review_data_df = pd.DataFrame(review_data)
+
+
+if st.button("Predict"):
+        
+    df = pd.DataFrame({
+           'review': [text], 
+           	       })
+    st.dataframe(df)
+    
+    result = model.predict(review_data_df['predict_sentiment'])[0]
+    if int(result) == 0:
+        result = "Negative"
+    else:
+        result = "Positive" 
+    print(result)
+    st.write(result)
+    
+    st.write("Success!")
+    st.balloons()
+    
+    
